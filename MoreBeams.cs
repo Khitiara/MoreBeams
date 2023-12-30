9fce8254675e85df34f43a0376ff9f2c0afa1599
Khitiara <robotbrainify@gmail.com>
2023-12-30 14:09:00 -0500

    fix switch etc placement on mod beams and document code

--- MoreBeams.cs
+++ MoreBeams.cs
@@ -11,39 +11,51 @@ namespace MoreBeams;
 public class MoreBeams : Mod
 {
 	private readonly List<int> _beamTilesAdded = new();
-	// private Dictionary<(int,int), int[]> _restoreTileData = new(5);
-	// private static readonly int[] vanillaTilesToModify = new[] {
-	// 	442,593,630,631,136,
-	// };
+
 	public override void Load() {
-		AddBeam("AshWood", DustID.Ash, ItemID.AshWood);
-		AddBeam("Ebonwood", DustID.Ebonwood, ItemID.Ebonwood);
-		AddBeam("Shadewood", DustID.Shadewood, ItemID.Shadewood);
-		AddBeam("PalmWood", DustID.PalmWood, ItemID.PalmWood);
-		AddBeam("DynastyWood", DustID.DynastyWood, ItemID.DynastyWood);
-		AddBeam("Pearlwood", DustID.Pearlwood, ItemID.Pearlwood);
-		AddBeam("SpookyWood", DustID.SpookyWood, ItemID.SpookyWood);
+		// AddBeam(beam_name, dust, material_id)
+		// Beam tile textures will be at Tiles/{beam_name}Beam.png
+		// Beam item texture will be at Items/{beam_name}BeamItem.png
+
+		AddBeam(nameof(ItemID.AshWood), DustID.Ash, ItemID.AshWood);
+		AddBeam(nameof(ItemID.Ebonwood), DustID.Ebonwood, ItemID.Ebonwood);
+		AddBeam(nameof(ItemID.Shadewood), DustID.Shadewood, ItemID.Shadewood);
+		AddBeam(nameof(ItemID.PalmWood), DustID.PalmWood, ItemID.PalmWood);
+		AddBeam(nameof(ItemID.DynastyWood), DustID.DynastyWood, ItemID.DynastyWood);
+		AddBeam(nameof(ItemID.Pearlwood), DustID.Pearlwood, ItemID.Pearlwood);
+		AddBeam(nameof(ItemID.SpookyWood), DustID.SpookyWood, ItemID.SpookyWood);
 
 		On_TileObjectData.isValidAlternateAnchor += OnTileObjectDataOnIsValidAlternateAnchor;
-
 	}
 
-	private bool OnTileObjectDataOnIsValidAlternateAnchor(On_TileObjectData.orig_isValidAlternateAnchor orig, TileObjectData self, int type) {
+	/// <summary>
+	/// Patch isValidAlternateAnchor to allow placing switches etc on mod beams.
+	/// </summary>
+	private static bool OnTileObjectDataOnIsValidAlternateAnchor(On_TileObjectData.orig_isValidAlternateAnchor orig, TileObjectData self, int type) {
 		bool ret = orig(self, type);
+		// only patch in ours if the vanilla thing makes an exception for vanilla beams
 		if (!ret && self.AnchorAlternateTiles.Contains(TileID.WoodenBeam)) {
-			return _beamTilesAdded.Contains(type);
+			// ModContent to avoid capturing mod
+			return ModContent.GetInstance<MoreBeams>()._beamTilesAdded.Contains(type);
 		}
         return ret;
 	}
 
+	/// <summary>
+	/// Add a new beam type
+	/// </summary>
+	/// <param name="name">
+	/// The name of the beam - tile textures will be at Tiles/{name}Beam.png and item texture will be at
+	/// Items/{name}BeamItem.png
+	/// </param>
+	/// <param name="dust">The dust ID to kick up when breaking the tile</param>
+	/// <param name="item">
+	/// The item ID of the material to craft the beam with. Recipe will always be 1 wood to 2 beams at the sawmill.
+	/// </param>
 	private void AddBeam(string name, short dust, short item) {
 		BeamTile tile = new($"{name}Beam", dust);
 		AddContent(tile);
 		_beamTilesAdded.Add(tile.Type);
 		AddContent(new BeamItem($"{name}BeamItem", item, tile.Type));
 	}
-
-	// public override void Unload() {
-	// 	base.Unload();
-	// }
 }
