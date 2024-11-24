19399180f153aefe17b0bd6521055e0718b1f0b0
Khitiara <robotbrainify@gmail.com>
2024-11-24 13:30:25 -0500

    MOAR BEAMS

--- Items/BeamItem.cs
+++ Items/BeamItem.cs
@@ -7,33 +7,48 @@ namespace MoreBeams.Items;
 public class BeamItem : ModItem
 {
     public override string Name { get; }
-    private readonly short   _baseWood;
-    private readonly ushort  _tileId;
-    private readonly bool    _isAncient;
+    private readonly short _baseWood;
+    private readonly ushort _tileId;
+    private readonly bool _isAncient;
     private readonly string? _ancientVariant;
+    private readonly bool _isLiving;
+    private readonly bool _useWoodGroup;
 
     protected override bool CloneNewInstances => true;
 
-    public BeamItem(string name, short baseWood, ushort tileId, bool isAncient = false, string? ancientVariant = null) {
+    public BeamItem(string name, short baseWood, ushort tileId, bool isAncient = false, string? ancientVariant = null,
+        bool isLiving = false, bool useWoodGroup = false)
+    {
         Name = name;
         _baseWood = baseWood;
         _tileId = tileId;
         _isAncient = isAncient;
         _ancientVariant = ancientVariant;
+        _isLiving = isLiving;
+        _useWoodGroup = useWoodGroup;
     }
 
-    public override void SetDefaults() {
+    public override void SetDefaults()
+    {
         Item.CloneDefaults(ItemID.WoodenBeam);
         Item.ResearchUnlockCount = 50;
         Item.createTile = _tileId;
     }
 
-    public override void AddRecipes() {
+    public override void AddRecipes()
+    {
         if (!_isAncient) {
-            CreateRecipe(2)
-                .AddIngredient(_baseWood)
-                .AddTile(TileID.Sawmill)
-                .Register();
+            if (_useWoodGroup) {
+                CreateRecipe(2)
+                    .AddRecipeGroup(RecipeGroupID.Wood)
+                    .AddTile(_isLiving ? TileID.LivingLoom : TileID.Sawmill)
+                    .Register();
+            } else {
+                CreateRecipe(2)
+                    .AddIngredient(_baseWood)
+                    .AddTile(_isLiving ? TileID.LivingLoom : TileID.Sawmill)
+                    .Register();
+            }
         }
 
         if (_ancientVariant is not null) {
@@ -41,8 +56,9 @@ public class BeamItem : ModItem
         }
     }
 
-    public override void Unload() {
-        if (_ancientVariant is not null) {
+    public override void Unload()
+    {
+        if (_ancientVariant is not null && Type < ItemID.Sets.ShimmerTransformToItem.Length) {
             ItemID.Sets.ShimmerTransformToItem[Type] = 0;
         }
     }
--- MoreBeams.cs
+++ MoreBeams.cs
@@ -17,101 +17,136 @@ namespace MoreBeams;
 [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]
 public sealed class MoreBeams : Mod
 {
-	private readonly  List<int>               _beamTilesAdded = new();
-	internal readonly Dictionary<string, int> BeamItems       = new();
-	private readonly  Dictionary<string, int> _beamTiles       = new();
-	private           Asset<Texture2D>[]      _backupGlows    = null!;
+    private readonly List<int> _beamTilesAdded = new();
+    internal readonly Dictionary<string, int> BeamItems = new();
+    private readonly Dictionary<string, int> _beamTiles = new();
+    private Asset<Texture2D>[] _backupGlows = null!;
 
-	public override void Load() {
-		// AddBeam(beam_name, dust, material_id)
-		// Beam tile textures will be at Tiles/{beam_name}Beam.png
-		// Beam item texture will be at Items/{beam_name}BeamItem.png
+    public override void Load()
+    {
+        // AddBeam(beam_name, dust, material_id)
+        // Beam tile textures will be at Tiles/{beam_name}Beam.png
+        // Beam item texture will be at Items/{beam_name}BeamItem.png
 
-		AddBeam(nameof(ItemID.AshWood), DustID.Ash, ItemID.AshWood);
-		AddBeam(nameof(ItemID.Ebonwood), DustID.Ebonwood, ItemID.Ebonwood);
-		AddBeam(nameof(ItemID.Shadewood), DustID.Shadewood, ItemID.Shadewood);
-		AddBeam(nameof(ItemID.PalmWood), DustID.PalmWood, ItemID.PalmWood);
-		AddBeam(nameof(ItemID.DynastyWood), DustID.DynastyWood, ItemID.DynastyWood, true, "NewDynastyWood");
-		AddBeam(nameof(ItemID.Pearlwood), DustID.Pearlwood, ItemID.Pearlwood);
-		AddBeam(nameof(ItemID.SpookyWood), DustID.SpookyWood, ItemID.SpookyWood, true, "NewSpookyWood");
-		AddBeam("FancyDynastyWood", DustID.DynastyWood, ItemID.DynastyWood);
-		AddBeam("NewDynastyWood", DustID.DynastyWood, ItemID.DynastyWood, ancientVariant: "DynastyWood");
-		AddBeam("NewSpookyWood", DustID.SpookyWood, ItemID.SpookyWood, ancientVariant: "SpookyWood");
-		AddBeam(nameof(ItemID.GrayBrick), DustID.Stone, ItemID.GrayBrick);
-		AddBeam(nameof(ItemID.StoneSlab), DustID.Stone, ItemID.StoneSlab);
-		AddBeam("Laser", DustID.Electric, ItemID.MartianConduitPlating);
+        AddBeam(nameof(ItemID.AshWood), DustID.Ash, ItemID.AshWood);
+        AddBeam(nameof(ItemID.Ebonwood), DustID.Ebonwood, ItemID.Ebonwood);
+        AddBeam(nameof(ItemID.Shadewood), DustID.Shadewood, ItemID.Shadewood);
+        AddBeam(nameof(ItemID.PalmWood), DustID.PalmWood, ItemID.PalmWood);
+        AddBeam(nameof(ItemID.DynastyWood), DustID.DynastyWood, ItemID.DynastyWood, true, "NewDynastyWood");
+        AddBeam(nameof(ItemID.Pearlwood), DustID.Pearlwood, ItemID.Pearlwood);
+        AddBeam(nameof(ItemID.SpookyWood), DustID.SpookyWood, ItemID.SpookyWood, true, "NewSpookyWood");
+        AddBeam("FancyDynastyWood", DustID.DynastyWood, ItemID.DynastyWood);
+        AddBeam("NewDynastyWood", DustID.DynastyWood, ItemID.DynastyWood, ancientVariant: "DynastyWood");
+        AddBeam("NewSpookyWood", DustID.SpookyWood, ItemID.SpookyWood, ancientVariant: "SpookyWood");
+        AddBeam(nameof(ItemID.GrayBrick), DustID.Stone, ItemID.GrayBrick);
+        AddBeam(nameof(ItemID.StoneSlab), DustID.Stone, ItemID.StoneSlab);
+        AddBeam("Laser", DustID.Electric, ItemID.MartianConduitPlating);
+        AddBeam("Stone", DustID.Stone, ItemID.StoneBlock);
+        AddBeam("Ice", DustID.Ice, ItemID.IceBlock);
+        AddBeam(nameof(ItemID.Glass), DustID.Glass, ItemID.Glass);
+        AddBeam("Bubblegum", DustID.Confetti, ItemID.BubblegumBlock);
 
-		AddBeam(nameof(ItemID.CopperBrick), DustID.Copper, ItemID.CopperBrick);
-		AddBeam(nameof(ItemID.GoldBrick), DustID.Gold, ItemID.GoldBrick);
-		AddBeam(nameof(ItemID.LeadBrick), DustID.Lead, ItemID.LeadBrick);
-		AddBeam(nameof(ItemID.IronBrick), DustID.Iron, ItemID.IronBrick);
-		AddBeam(nameof(ItemID.ObsidianBrick), DustID.Obsidian, ItemID.ObsidianBrick);
-		AddBeam(nameof(ItemID.PlatinumBrick), DustID.Platinum, ItemID.PlatinumBrick);
-		AddBeam(nameof(ItemID.SilverBrick), DustID.Silver, ItemID.SilverBrick);
-		AddBeam(nameof(ItemID.TinBrick), DustID.Tin, ItemID.TinBrick);
-		AddBeam(nameof(ItemID.TungstenBrick), DustID.Tungsten, ItemID.TungstenBrick);
+        AddBeam("LivingLeaf", DustID.t_LivingWood, ItemID.Wood, isLiving: true, useWoodGroup: true);
+        AddBeam("LivingWood", DustID.t_LivingWood, ItemID.Wood, isLiving: true, useWoodGroup: true);
+        AddBeam("LivingMahogany", DustID.t_LivingWood, ItemID.RichMahogany, isLiving: true);
+        AddBeam("MahoganyLeaf", DustID.t_LivingWood, ItemID.RichMahogany, isLiving: true);
 
-		AddBeam("Titanstone", DustID.Titanium, ItemID.TitanstoneBlock);
+        AddBeam(nameof(ItemID.CopperBrick), DustID.Copper, ItemID.CopperBrick);
+        AddBeam(nameof(ItemID.GoldBrick), DustID.Gold, ItemID.GoldBrick);
+        AddBeam(nameof(ItemID.LeadBrick), DustID.Lead, ItemID.LeadBrick);
+        AddBeam(nameof(ItemID.IronBrick), DustID.Iron, ItemID.IronBrick);
+        AddBeam(nameof(ItemID.ObsidianBrick), DustID.Obsidian, ItemID.ObsidianBrick);
+        AddBeam(nameof(ItemID.PlatinumBrick), DustID.Platinum, ItemID.PlatinumBrick);
+        AddBeam(nameof(ItemID.SilverBrick), DustID.Silver, ItemID.SilverBrick);
+        AddBeam(nameof(ItemID.TinBrick), DustID.Tin, ItemID.TinBrick);
+        AddBeam(nameof(ItemID.TungstenBrick), DustID.Tungsten, ItemID.TungstenBrick);
+        AddBeam(nameof(ItemID.MeteoriteBrick), DustID.Meteorite, ItemID.Meteorite);
+        AddBeam(nameof(ItemID.CobaltBrick), DustID.Cobalt, ItemID.CobaltBrick);
+        AddBeam(nameof(ItemID.MythrilBrick), DustID.Mythril, ItemID.MythrilBrick);
+        AddBeam(nameof(ItemID.AdamantiteBeam), DustID.Adamantite, ItemID.AdamantiteBeam);
+        AddBeam(nameof(ItemID.PalladiumColumn), DustID.Palladium, ItemID.PalladiumColumn);
+        AddBeam(nameof(ItemID.ChlorophyteBrick), DustID.Chlorophyte, ItemID.ChlorophyteBrick);
+        AddBeam("LuminiteBrick", DustID.LunarOre, ItemID.LunarBrick);
 
-		On_TileObjectData.isValidAlternateAnchor += OnTileObjectDataOnIsValidAlternateAnchor;
-	}
+        AddBeam(nameof(ItemID.CrimtaneBrick), DustID.Crimstone, ItemID.CrimtaneBrick);
+        AddBeam(nameof(ItemID.DemoniteBrick), DustID.Demonite, ItemID.DemoniteBrick);
 
-	public override void PostSetupContent() {
-		_backupGlows = TextureAssets.GlowMask;
-		int end = GlowMaskID.Count;
-		Array.Resize(ref TextureAssets.GlowMask, TextureAssets.GlowMask.Length + 1);
-		AddGlow("Laser", ref end);
-	}
+        AddBeam("Eaterof", DustID.Corruption, ItemID.RottenChunk);
+        AddBeam("Spinal", DustID.Crimson, ItemID.Vertebrae);
 
-	/// <summary>
-	/// Patch isValidAlternateAnchor to allow placing switches etc on mod beams.
-	/// </summary>
-	private static bool OnTileObjectDataOnIsValidAlternateAnchor(On_TileObjectData.orig_isValidAlternateAnchor orig, TileObjectData self, int type) {
-		bool ret = orig(self, type);
-		// only patch in ours if the vanilla thing makes an exception for vanilla beams
-		if (!ret && self.AnchorAlternateTiles.Contains(TileID.WoodenBeam)) {
-			// ModContent to avoid capturing mod
-			return ModContent.GetInstance<MoreBeams>()._beamTilesAdded.Contains(type);
-		}
+        AddBeam("Titanstone", DustID.Titanium, ItemID.TitanstoneBlock);
+        AddBeam(nameof(ItemID.ShroomitePlating), DustID.GlowingMushroom, ItemID.ShroomitePlating);
+
+        On_TileObjectData.isValidAlternateAnchor += OnTileObjectDataOnIsValidAlternateAnchor;
+    }
+
+    public override void PostSetupContent()
+    {
+        _backupGlows = TextureAssets.GlowMask;
+        int end = GlowMaskID.Count;
+        Array.Resize(ref TextureAssets.GlowMask, TextureAssets.GlowMask.Length + 1);
+        AddGlow("Laser", ref end);
+    }
+
+    /// <summary>
+    /// Patch isValidAlternateAnchor to allow placing switches etc on mod beams.
+    /// </summary>
+    private static bool OnTileObjectDataOnIsValidAlternateAnchor(On_TileObjectData.orig_isValidAlternateAnchor orig,
+        TileObjectData self, int type)
+    {
+        bool ret = orig(self, type);
+        // only patch in ours if the vanilla thing makes an exception for vanilla beams
+        if (!ret && self.AnchorAlternateTiles.Contains(TileID.WoodenBeam)) {
+            // ModContent to avoid capturing mod
+            return ModContent.GetInstance<MoreBeams>()._beamTilesAdded.Contains(type);
+        }
+
         return ret;
-	}
+    }
 
-	/// <summary>
-	/// Add a new beam type
-	/// </summary>
-	/// <param name="name">
-	///     The name of the beam - tile textures will be at Tiles/{name}Beam.png and item texture will be at
-	///     Items/{name}BeamItem.png
-	/// </param>
-	/// <param name="dust">The dust ID to kick up when breaking the tile</param>
-	/// <param name="item">
-	///     The item ID of the material to craft the beam with. Recipe will always be 1 wood to 2 beams at the sawmill.
-	/// </param>
-	/// <param name="isAncient"></param>
-	/// <param name="ancientVariant"></param>
-	private void AddBeam(string name, short dust, short item, bool isAncient = false, string? ancientVariant = null) {
-		BeamTile tile = new($"{name}Beam", dust);
-		AddContent(tile);
-		_beamTilesAdded.Add(tile.Type);
-		_beamTiles[name] = tile.Type;
-		BeamItem beamItem = new($"{name}BeamItem", item, tile.Type, isAncient, ancientVariant);
-		AddContent(beamItem);
-		BeamItems[name] = beamItem.Type;
-	}
+    /// <summary>
+    /// Add a new beam type
+    /// </summary>
+    /// <param name="name">
+    ///     The name of the beam - tile textures will be at Tiles/{name}Beam.png and item texture will be at
+    ///     Items/{name}BeamItem.png
+    /// </param>
+    /// <param name="dust">The dust ID to kick up when breaking the tile</param>
+    /// <param name="item">
+    ///     The item ID of the material to craft the beam with. Recipe will always be 1 wood to 2 beams at the sawmill.
+    /// </param>
+    /// <param name="isAncient"></param>
+    /// <param name="ancientVariant"></param>
+    /// <param name="isLiving"></param>
+    /// <param name="useWoodGroup"></param>
+    private void AddBeam(string name, short dust, short item, bool isAncient = false, string? ancientVariant = null,
+        bool isLiving = false, bool useWoodGroup = false)
+    {
+        BeamTile tile = new($"{name}Beam", dust);
+        AddContent(tile);
+        _beamTilesAdded.Add(tile.Type);
+        _beamTiles[name] = tile.Type;
+        BeamItem beamItem = new($"{name}BeamItem", item, tile.Type, isAncient, ancientVariant, isLiving, useWoodGroup);
+        AddContent(beamItem);
+        BeamItems[name] = beamItem.Type;
+    }
 
 
-	private void AddGlow(string tile, ref int idx) {
-		int beamTile = _beamTiles[tile];
-		Logger.Debug($"Beam {tile} is {beamTile}");
-		Main.tileGlowMask[beamTile] = (short)idx;
-		Logger.Debug($"Set tileGlowMask for {tile}, adding glowmask to textureassets array");
-		TextureAssets.GlowMask[idx++] = ModContent.Request<Texture2D>(ModContent.GetModTile(beamTile).Texture + "_Glow");
-	}
+    private void AddGlow(string tile, ref int idx)
+    {
+        int beamTile = _beamTiles[tile];
+        Logger.Debug($"Beam {tile} is {beamTile}");
+        Main.tileGlowMask[beamTile] = (short)idx;
+        Logger.Debug($"Set tileGlowMask for {tile}, adding glowmask to textureassets array");
+        TextureAssets.GlowMask[idx++] =
+            ModContent.Request<Texture2D>(ModContent.GetModTile(beamTile).Texture + "_Glow");
+    }
 
-	public override void Unload() {
-		TextureAssets.GlowMask = _backupGlows;
-		_beamTilesAdded.Clear();
-		BeamItems.Clear();
-		_beamTiles.Clear();
-	}
+    public override void Unload()
+    {
+        TextureAssets.GlowMask = _backupGlows;
+        _beamTilesAdded.Clear();
+        BeamItems.Clear();
+        _beamTiles.Clear();
+    }
 }
